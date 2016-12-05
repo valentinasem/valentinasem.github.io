ae5862fd6b3f8ac162cb1db7cfd2181e9afbd933
Valentina Semenikhina <valentina.sem@gmail.com>
2016-12-05 17:55:23 +0000

    My first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+smoothScroll.init();
+
+ // $('.owl-carousel').owlCarousel({
+ // 	items: 1,
+ // 	margin: 10,
+ // 	loop: true,
+ // 	// autoplay: true,
+ // 	// autoplayTimeout: 2000,
+ // 	// autoplaySpeed: 600
+ // });
+
+ 
